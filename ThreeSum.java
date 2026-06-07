67fa35b961fc380a91826e408e87674aec3680a0
asbhandari012 <asbhandari012@gmail.com>
2026-06-07 14:44:57 +0530

    ThreeSum solution

---
+++ ThreeSum.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class ThreeSum {
+
+    public static List<List<Integer>> threeSum(int nums[]) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    List<Integer> temp = new ArrayList<>();
+
+                    temp.add(nums[i]);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+
+                    ans.add(temp);
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        // Scanner ui = new Scanner(System.in);
+
+        int nums[] = { -1, 0, 1, 2, -1, -4 };
+
+        List<List<Integer>> result = threeSum(nums);
+        System.out.println(result);
+
+    }
+}
