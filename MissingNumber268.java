48c71d9f16e8efb1c8e57687c331992ac0bba164
asbhandari012 <asbhandari012@gmail.com>
2026-06-09 00:39:06 +0530

    Another question solved

---
+++ MissingNumber268.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class MissingNumber268 {
+
+    public static int missingNumber(int[] nums) {
+
+        Arrays.sort(nums);
+
+        if (nums[0] != 0) {
+            return 0;
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] != nums[i] + 1) {
+                return nums[i] + 1;
+            }
+        }
+
+        return nums.length;
+    }
+
+    public static void main(String args) {
+
+        int nums[] = { 3, 0, 1 };
+
+        System.out.println(missingNumber(nums));
+    }
+
+}
